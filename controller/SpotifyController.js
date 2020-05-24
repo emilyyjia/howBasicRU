fb70135e4544908b6abc8dae5caed37e0d655d13
emilyjia <emilyjia2010@gmail.com>
2020-05-23 23:08:44 -0700

    added calculations

--- controller/SpotifyController.js
+++ controller/SpotifyController.js
@@ -1,8 +1,11 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+var querystring = require('querystring');
 
 var client_id = ''; // Take out when showing others
 var client_secret = ''; // Take out when showing others
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var stateKey = 'spotify_auth_state'
+var globalTopFiftyId = '37i9dQZEVXbMDoHDwVN2tF';
 
 var spotifyApi = new SpotifyWebApi({
     clientId: client_id,
@@ -17,7 +20,7 @@ module.exports.loginSpotify = function(res) {
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
 
-    var scope = 'user-read-private user-read-email user-top-read';
+    var scope = 'user-top-read';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
@@ -40,9 +43,43 @@ module.exports.callbackSpotify = function(req, res) {
 
     }).then(function(data) {
         spotifyApi.getMyTopArtists({
-            limit: 20
-        }).then(function(data) {
-            var topArtists = data.body.items;
+            limit: 15
+        })
+        .then(function(data) {
+            req.session.myTopArtists = data.body.items.map(function(a, i) {
+                var name = a.name;
+                console.log(i + 1 + ": " + name);
+                return name;
+            })
+            return data.body.items.map(function(a) {
+                return a.id;
+            });
+        })
+        .then(function(artistIds) {
+            spotifyApi.getPlaylistTracks(globalTopFiftyId)
+            .then(function(data) {
+                tracks = data.body.items.map(function(t) {
+                    console.log(t.track.name);
+                    return t.track;
+                });
+                var topFiftyArtistIds = [];
+                tracks.forEach(function(t) {
+                    t.artists.forEach(function(a) {
+                        topFiftyArtistIds.push(a.id);
+                    });
+                });
+                var numMatches = 0;
+                artistIds.forEach(function(arid) {
+                    if (topFiftyArtistIds.includes(arid)) {
+                        numMatches++;
+                    }
+                })
+                console.log("Number of matches: " + numMatches);
+                req.session.fraction = numMatches + "/" + artistIds.length;
+                var basicality = numMatches/artistIds.length;
+                req.session.basicality = basicality;
+                res.redirect('/howBasicRU');
+            })
         });
     });
 }
@@ -61,5 +98,4 @@ var generateRandomString = function(length) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-  };
-  
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var spotifyController = require('../controller/SpotifyController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'How basic are you?' });
+});
+
+router.get('/howBasicRU', function(req, res, next) {
+  var fraction = req.session.fraction;
+  var basicality = req.session.basicality;
+  var topArtists = req.session.myTopArtists;
+  console.log("length: " + topArtists.length);
+  res.render('result', {
+    title: 'results',
+    fraction: fraction,
+    basicality: basicality,
+    topArtists: topArtists
+  })
+});
+
+router.get('/callback', function(req, res) { 
+  spotifyController.callbackSpotify(req, res)
+});
+
+router.get('/login', function(req, res) {
+  spotifyController.loginSpotify(res)
 });
 
 module.exports = router;
