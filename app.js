ca473b91bc1c1a66654d03a0d3d6cb82fca9aece
emilyjia <emilyjia2010@gmail.com>
2020-06-07 22:19:09 -0700

    switched to cookie-session for now

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session')
+// var session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+var cookieSession = require('cookie-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,11 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
+// app.use(session({
+//   secret: 'shhh',
+//   resave: false,
+//   saveUninitialized: false,
+//   store: new MongoStore({ url: ''})
+// }));
+app.use(cookieSession({
   secret: 'shhh',
-  resave: false,
-  saveUninitialized: false
-}));
+  maxAge: 1000*60*60
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
